2e1cb28ee27bc9125ab813bf783c5880e5da9733
sabbirsami <smd71430@gmail.com>
2025-07-16 22:07:39 +0600

    fix menu

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 'use client';
+import FacebookIcon from '@/components/icons/FacebookIcon';
+import InstagramIcon from '@/components/icons/InstagramIcon';
 import PlusIcon from '@/components/icons/PlusIcon';
 import BookingSheet from '@/components/ui/booking-sheet';
 import {
@@ -19,14 +21,24 @@ const Navbar = () => {
   const pathname = usePathname();
   const navLinks = [
     { href: '/', label: 'Home' },
-    { href: '/our-story', label: 'Our Story' },
-    { href: '/career', label: 'Career' },
-    { href: '/faq', label: 'FAQs' },
+    { href: '/menus', label: 'Il Menu (menus)' },
+    { href: '/events', label: 'Events' },
+    { href: '/history', label: 'La Storia (history)' },
+    { href: '/impressions', label: 'Impressions (Bildgalerie)' },
+    { href: '/contact', label: 'Contatto (contact / opening hours)' },
+    { href: '/jobs', label: 'Jobs' },
   ];
-  const socialLinks = [
-    { href: 'https://www.instagram.com/', label: 'Instagram' },
-    { href: 'https://www.facebook.com/', label: 'Facebook' },
-  ];
+  // const socialLinks = [
+  //   { href: 'https://www.instagram.com/', label: 'Instagram' },
+  //   { href: 'https://www.facebook.com/', label: 'Facebook' },
+  // ];
+  const processText = (text: string) => {
+    const parts = text.split('(');
+    const mainText = parts[0].trim();
+    const bracketed = parts[1] ? `(${parts[1]}` : '';
+
+    return { mainText, bracketed };
+  };
 
   return (
     <header className="flex items-center px-6 bg-white sticky top-0 z-50 w-full">
@@ -51,25 +63,36 @@ const Navbar = () => {
               <SheetDescription></SheetDescription>
               <div
                 style={{ height: 'calc(100vh - 4rem)' }}
-                className="w-full flex gap-1.5 flex-col h-full items-start justify-center px-20"
+                className="w-full flex gap-0.5 flex-col h-full items-center justify-center ps-16 pe-10"
               >
-                <div className="flex items-center mb-4 gap-4">
+                {/* <div className="flex items-center mb-4 gap-4">
                   <h5 className="text-xl mb-2 font-narrow">Menu</h5>
                   <div className="w-8 h-[1px] bg-gray-600"></div>
-                </div>
-                {navLinks.map((link) => (
-                  <Link
-                    key={link.href}
-                    href={link.href}
-                    onClick={() => setIsOpen(false)}
-                    className={`block text-5xl font-medium text-center relative after:absolute after:bottom-0 after:left-0 after:h-[3px] after:w-full after:origin-bottom-right after:scale-x-0 dark:after:bg-black after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 hover:italic ${
-                      pathname === link.href ? 'italic after:scale-x-100' : ''
-                    }`}
-                  >
-                    {link.label}
-                  </Link>
-                ))}
-                <div className="flex items-center mt-10 mb-4 gap-4">
+                </div> */}
+                {navLinks.map((link) => {
+                  const { mainText, bracketed } = processText(link.label);
+
+                  return (
+                    <Link
+                      key={link.href}
+                      href={link.href}
+                      onClick={() => setIsOpen(false)}
+                      className={`block font-medium text-center relative after:absolute after:bottom-1.5 after:left-0 after:h-[3px] after:w-full after:origin-bottom-right after:scale-x-0 dark:after:bg-black after:bg-neutral-800 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 hover:italic  ${
+                        pathname === link.href ? 'italic after:scale-x-100' : ''
+                      }`}
+                    >
+                      <div className="flex items-center gap-2.5">
+                        <span className="text-5xl leading-tight">{mainText}</span>
+                        {bracketed && (
+                          <span className="text-xl uppercase leading-tight">
+                            {bracketed.split('(')[1].split(')')[0]}
+                          </span>
+                        )}
+                      </div>
+                    </Link>
+                  );
+                })}
+                {/* <div className="flex items-center mt-10 mb-4 gap-4">
                   <h5 className="text-xl mb-2 font-narrow">Social</h5>
                   <div className="w-8 h-[1px] bg-gray-600"></div>
                 </div>
@@ -84,7 +107,7 @@ const Navbar = () => {
                   >
                     {link.label}
                   </Link>
-                ))}
+                ))} */}
               </div>
             </SheetHeader>
           </div>
@@ -98,15 +121,15 @@ const Navbar = () => {
       </div>
       {/* Right side content */}
       <div className="flex items-center ml-auto z-20">
-        {/* <div className="flex items-center gap-2 pe-3">
+        <div className="flex items-center gap-2 pe-3">
           <Link href={'https://www.google.com'} className="text-primary">
             <InstagramIcon />
           </Link>
           <Link href={'https://www.google.com'} className="text-primary">
             <FacebookIcon />
           </Link>
-        </div> */}
-        {/* <div className="w-4 h-[1px] bg-gray-600"></div> */}
+        </div>
+        <div className="w-4 h-[1px] bg-gray-600"></div>
         <div className="text-2xl flex items-center gap-1 px-3">
           <button className="hover:text-amber-500">De</button>,
           <button className="hover:text-amber-500">En</button>
