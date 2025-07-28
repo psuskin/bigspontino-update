7203481456be4b5299eebcb69e3578cca893c0ca
sabbirsami <smd71430@gmail.com>
2025-07-28 09:31:31 +0600

    remove consulting from footer

--- components/shared/footer/Footer.tsx
+++ components/shared/footer/Footer.tsx
@@ -138,14 +138,14 @@ const Footer = () => {
                   {t('footer.contact')}
                 </Link>
               </motion.li>
-              <motion.li variants={linkVariants}>
+              {/* <motion.li variants={linkVariants}>
                 <Link
                   href="/consulting"
                   className="relative text-xs sm:text-sm text-gray-300 font-medium after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-gray-300 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100"
                 >
                   {t('footer.consulting')}
                 </Link>
-              </motion.li>
+              </motion.li> */}
               <motion.li variants={linkVariants}>
                 <Link
                   href="/history"
